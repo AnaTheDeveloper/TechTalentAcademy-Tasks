88ae29e128bbb081fb800947a814c0444968e092
Ana Edwards <edwards.ana81@gmail.com>
2022-02-17 13:18:44 +0000

    Data Visualisation Notes on Graph Types Part 1

--- PandasDataVisualisationNotesPart1.py
+++ PandasDataVisualisationNotesPart1.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 # In Pandas, you can also create visualisations. These include:
 # • Line Plot
@@ -13,6 +14,30 @@ import matplotlib.pyplot as plt
 # • Scatter Matrix Plot
 
 if __name__ == '__main__':
+
+    # -------------------Tips & Tricks---------------- #
+
+    # Import matplotlib.pyplot as plt
+
+    # Create a Title for graph: plt.title("Hotels and Scores")
+
+    # Create a x axis label: plt.xlabel("Number of Hotels")
+
+    # Create a y axis label: plt.ylabel("Score")
+
+    # Scatter graph: dataSetName.plot()/.line()/.bar()/.barh()/.pie()   (x='nameOfDataSetSection', y='nameOfOtherDataSetSection')
+
+    # Chart Customisation
+    # Determine graph type: kind='scatter'
+    # Change colour: color='red' or '#143sdf34'
+    # Change figure size: figsize=(7, 7),
+    # Add a percentage value: autopct='%1.1f%%'
+    # Everything is rotated counter-clockwise by 90 degrees: startangle=90
+    # Rotate Text: rotation=45
+
+
+
+
 
     # Scatter graph
     gdp_cal = pd.DataFrame({
@@ -86,6 +111,78 @@ if __name__ == '__main__':
 # from pandas.plotting import scatter_matrix
 # scatter_matrix(df, figsize=(10, 10))
 
+# ---------------------EXAMPLE CHARTS FROM STACK OVERFLOW------------------------#
+
+    cols = ['Date', 'Brake Hrs',  'Tram Hrs',  'Brake%',  'Tram%',  'Br/Tr%']
+    data_frame = pd.DataFrame([['Mon',       0.87,      4.26,   16.90,  83.10,   20.33],
+                                ['Tue',       1.00,      3.05,   24.66,  75.34,   32.73],
+                                ['Wed',       1.77,      3.87,   31.44,  68.56,   45.85],
+                                ['Thu',       1.86,      5.16,   26.44,  73.56,   35.94],
+                                ['Fri',       1.41,      2.01,   41.15,  58.85,   69.93],
+                                ['Sat',       0.01,      5.03,    0.14,  99.86,    0.14],
+                                ['Sun',       0.40,      1.16,   25.82,  74.18,   34.82]],
+                             columns = cols)
+
+
+
+    f, ax1 = plt.subplots(1, figsize=(10, 5))
+
+    # Set the bar width
+    bar_width_tram = 1
+    bar_width_brake_percentage = 1
+
+    # Positions of the left bar-boundaries
+    bar_l = [i + 1 for i in range(len(data_frame['Tram Hrs']))]
+
+    # Positions of the x-axis ticks (center of the bars as bar labels)
+    tick_pos = [i + (bar_width_tram / 2) for i in bar_l]
+
+    # Define the colours for the corresponding
+    # bars in the graph
+    tram_data_hrs_colour = '#00AC00'   # Green
+    bd_per_colour = '#DA0505'   # Red
+
+    # Create a bar plot, in position bar_1
+    ax1.bar(bar_l,
+            # using the pre_score data
+            data_frame['Tram Hrs'],
+            # get rid of border lines
+            edgecolor="none",
+            # set the width
+            width=bar_width_tram,
+            # with the label pre score
+            label='Tramming Hours',
+            # with alpha 0.5
+            alpha=0.05,
+            # with color
+            color=tram_data_hrs_colour,
+            align = 'center')
+
+
+    # Set the X-ticks with dates
+    plt.xticks(tick_pos, data_frame['Date'])
+
+    plt.yticks(np.arange(0, 15, 3))
+
+    # Set the labels and legend
+    ax1.set_ylabel("Tramming Time (Hours)")
+    ax1.set_xlabel("Previous Week")
+
+    # Second axis
+    ax2 = ax1.twinx()
+    ax2.set_ylabel("Braking to Tramming  (%)")
+
+    plt.plot(bar_l,data_frame['Brake%'], '-', label='Relative Braking Percentage')
+
+    plt.yticks(np.arange(0, 250, 50))
+
+    # Set the title of the chart
+    h1, l1 = ax1.get_legend_handles_labels()
+    h2, l2 = ax2.get_legend_handles_labels()
+    ax1.legend(h1+h2, l1+l2, loc=2)
+
+    plt.xlim(0, len(tick_pos) + 1)
+    plt.show()
 
 
 
