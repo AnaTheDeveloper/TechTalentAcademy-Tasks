62a7110e172d35884e2ecf1edeaacd57be3658b0
Ana Edwards <edwards.ana81@gmail.com>
2022-02-23 17:48:25 +0000

    Dashboard task Completed

---
+++ TTAHW230222.py
@@ -0,0 +1,12 @@
+# Task 1 - Create your own dashboard. You can either use Altair, Tableau or Power Bi.
+
+# Use Kaggle for your appropriate datasets: https://www.kaggle.com/datasets
+# Check out the PowerBI community gallery for inspiration!: https://community.powerbi.com/t5/Data-Stories-Gallery/bdp/DataStoriesGaller
+
+# ---------------------------------SEE GIT READ ME FOR THE GRAPHS---------------------------------#
+# ---------------------------------PLEASE READ BELOW---------------------------------#
+
+# Used this dataset from Kaggle: https://www.kaggle.com/rohitsahoo/sales-forecasting
+
+# Link to my interactive dashboard on Tableau: https://public.tableau.com/app/profile/ana.edwards/viz/ProfitBashboardTTAHW/ProfitDashboard?publish=yes
+
