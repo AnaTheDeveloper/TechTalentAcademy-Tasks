0a8229a0fcad23778945719fad81d467751dd5a2
Ana Edwards <edwards.ana81@gmail.com>
2022-03-09 18:23:22 +0000

    Week 9 - Task 1 Completed / Task 2 In Progress

---
+++ TTAHW090322.py
@@ -0,0 +1,26 @@
+# Task 1
+# Practical: Artificial Intelligence (AI)
+
+# In Data Science we process a lot data through AI. With the General Data Protection Regulation (GDPR), it is becoming increasingly important to understand
+# the ethics behind the data that is collected, stored, processed and evaluated.
+
+# • Find out what Responsible AI is?
+# • Find instances where AI has failed? Or been used maliciously or incorrectly.
+# • Implications of when AI fails. There is a specific article in the GDPR Law that covers this, especially with
+#   automated decision making. (opt in and out options).
+# • What should organisations do to ensure that they are being responsible with AI and the wider use of data in general?
+# • Maximum 500 words.
+
+# ---------TO SEE THIS TASK PLEASE CLICK ON THE LINK BELOW---------------------------
+# https://docs.google.com/document/d/1pf6u9zN_R1wQaGNSpiNWI2anSxnfEKVr/edit?usp=sharing&ouid=104314020022454311060&rtpof=true&sd=true
+
+# IF LINK WAS UNSUCCESSFUL/DIDNT WORK PLEASE SEE GITHUB READ ME SECTION FOR WEEK 9!
+
+# Extension Task 1
+
+# Use the website to practice:https://machinelearningmastery.com/tensorflow-tut
+# The steps and code are very similar – you should start to notice the similarities yourselves.
+# This page features multiple neural network tutorials – if you chose to do this optional task, you only need
+# to pick one of these.
+
+if __name__ == '__main__':
